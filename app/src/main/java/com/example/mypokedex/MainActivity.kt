5f06ddceb32e1287e7999de913f8c02d7a4cc2f3
Shanilka Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-04-03 09:57:49 +0530

    initialiez compose-navigation and added hilt entrypoint annotation

--- app/src/main/java/com/example/mypokedex/MainActivity.kt
+++ app/src/main/java/com/example/mypokedex/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.mypokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.mypokedex.ui.theme.JetpackComposePokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                         //TODO
                     }
                     composable(
-                        "pokemon_detail_screen",
+                        "pokemon_detail_screen/{dominantColor}/{pokemonName}",
                         arguments = listOf(
                             navArgument("dominantColor"){
                                 type = NavType.IntType
@@ -33,6 +36,14 @@ class MainActivity : ComponentActivity() {
                         )
                     ){
                         //TODO
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt("dominantColor")
+                            color?.let { Color(it) } ?: Color.White
+                        }
+                        val pokemonName = remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+
                     }
                 }
             }
