cfaad100362fb2820fb38cd7ba7f8c2eb0e544c1
Shanilka Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-04-04 09:11:01 +0530

    added 2 GET Methods

--- app/src/main/java/com/example/mypokedex/repository/PokemonRepository.kt
+++ app/src/main/java/com/example/mypokedex/repository/PokemonRepository.kt
@@ -13,6 +13,22 @@ class PokemonRepository @Inject constructor(
     private val api : PokeApi
 ){
 
+    suspend fun getPokemonList(limit: Int, offset: Int): Resource<PokemonList>{
+        val response = try {
+            api.getPokemonList(limit, offset)
+        }catch (e: Exception){
+            return Resource.Error("An error occured")
+        }
+        return Resource.Success(response)
+    }
 
+    suspend fun getPokemonInfo(pokemonName: String): Resource<Pokemon>{
+        val response = try {
+            api.getPokemonInfo(pokemonName)
+        }catch (e: Exception){
+            return Resource.Error("An error occured")
+        }
+        return Resource.Success(response)
+    }
 
 }
