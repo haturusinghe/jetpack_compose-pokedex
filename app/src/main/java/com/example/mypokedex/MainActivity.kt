c6826a7d59af4bc5e54c241c35e828ac12cdff5c
Shanilka Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-04-05 09:09:56 +0530

    added navigation composable PokeMonDetailScreen

--- app/src/main/java/com/example/mypokedex/MainActivity.kt
+++ app/src/main/java/com/example/mypokedex/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.mypokedex
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.toLowerCase
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.mypokedex.pokemondetail.PokemonDetailScreen
 import com.example.mypokedex.pokemonlist.PokemonListScreen
 import com.example.mypokedex.ui.theme.JetpackComposePokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -21,22 +26,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposePokedexTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "pokemon_list_screen" ){
-                    composable("pokemon_list_screen"){
+                NavHost(navController = navController, startDestination = "pokemon_list_screen") {
+                    composable("pokemon_list_screen") {
                         //TODO
                         PokemonListScreen(navController = navController)
                     }
                     composable(
-                        "pokemon_detail_screen/{dominantColor}/{pokemonName}",
+                        "pokemon_details_screen/{dominantColor}/{pokemonName}",
                         arguments = listOf(
-                            navArgument("dominantColor"){
+                            navArgument("dominantColor") {
                                 type = NavType.IntType
                             },
-                            navArgument("pokemonName"){
+                            navArgument("pokemonName") {
                                 type = NavType.StringType
                             }
                         )
-                    ){
+                    ) {
                         //TODO
                         val dominantColor = remember {
                             val color = it.arguments?.getInt("dominantColor")
@@ -45,6 +50,12 @@ class MainActivity : ComponentActivity() {
                         val pokemonName = remember {
                             it.arguments?.getString("pokemonName")
                         }
+                        Timber.i("Pokemon Name: ${pokemonName}")
+                        PokemonDetailScreen(
+                            dominantColor = dominantColor,
+                            pokemonName = pokemonName?.lowercase(Locale.ROOT) ?: "",
+                            navController = navController
+                        )
 
                     }
                 }
