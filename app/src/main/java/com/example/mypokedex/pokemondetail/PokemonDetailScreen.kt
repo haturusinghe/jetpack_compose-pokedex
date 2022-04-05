028ef4803f002cf3ae381d4c3f3b94b1912075ef
Shanilka Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-04-05 09:10:56 +0530

    PokemonDetail - StateWrapper added

--- app/src/main/java/com/example/mypokedex/pokemondetail/PokemonDetailScreen.kt
+++ app/src/main/java/com/example/mypokedex/pokemondetail/PokemonDetailScreen.kt
@@ -1,15 +1,22 @@
 package com.example.mypokedex.pokemondetail
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -22,6 +29,7 @@ import com.example.mypokedex.util.Resource
 import com.skydoves.landscapist.CircularReveal
 import com.skydoves.landscapist.glide.GlideImage
 import com.skydoves.landscapist.palette.BitmapPalette
+import timber.log.Timber
 
 @Composable
 fun PokemonDetailScreen(
@@ -36,6 +44,34 @@ fun PokemonDetailScreen(
         value = viewModel.getPokemonInfo(pokemonName)
     }.value
 
+    PokemonDetailTopSection(
+        navController = navController,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.2f)
+    )
+    PokemonDetailStateWrapper(
+        pokemonInfo = pokemonInfo,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                top = topPadding + pokemonImageSize / 2f,
+                start = 16.dp,
+                end = 16.dp,
+                bottom = 16.dp
+            )
+            .shadow(10.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .background(MaterialTheme.colors.background)
+            .padding(16.dp), loadingModifier = Modifier
+            .padding(
+                top = topPadding + pokemonImageSize / 2f,
+                start = 16.dp,
+                end = 16.dp,
+                bottom = 16.dp
+            )
+            .size(100.dp)
+    )
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -44,14 +80,14 @@ fun PokemonDetailScreen(
     ) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
             if (pokemonInfo is Resource.Success) {
-                pokemonInfo.data?.sprites?.let {
+                pokemonInfo.data?.sprites?.front_default?.let {
                     GlideImage(
                         imageModel = it,
                         contentDescription = pokemonInfo.data.name,
                         circularReveal = CircularReveal(),
                         modifier = Modifier
                             .size(pokemonImageSize)
-                            .offset(y = topPadding)
+
                     )
                 }
             }
@@ -61,9 +97,10 @@ fun PokemonDetailScreen(
 
 @Composable
 fun PokemonDetailTopSection(navController : NavController, modifier : Modifier) {
+
     Box(
-        contentAlignment = Alignment.TopCenter,
-        modifier = Modifier.background(
+        contentAlignment = Alignment.TopStart,
+        modifier = modifier.background(
             Brush.verticalGradient(
                 listOf(
                     Color.Black,
@@ -82,6 +119,22 @@ fun PokemonDetailTopSection(navController : NavController, modifier : Modifier) 
 }
 
 @Composable
-fun PokemonDetailStateWrapper() {
+fun PokemonDetailStateWrapper(
+    pokemonInfo : Resource<Pokemon>,
+    modifier : Modifier = Modifier,
+    loadingModifier : Modifier = Modifier
+) {
+    when (pokemonInfo) {
+        is Resource.Success -> {
+            Text(text = pokemonInfo.data?.name ?: "", modifier = modifier)
+        }
+        is Resource.Error -> {
+            
+            Text(text = "Error", modifier = modifier)
+        }
+        is Resource.Loading -> {
 
+            CircularProgressIndicator(modifier = loadingModifier)
+        }
+    }
 }
